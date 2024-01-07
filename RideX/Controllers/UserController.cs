741e31c16a020acdfc67e43461be7e8b640d6e21
sabastinesobo <sobosabastine17prj@gmail.com>
2024-01-07 22:07:13 +0000

    fixing UserController error

--- RideX/Controllers/UserController.cs
+++ RideX/Controllers/UserController.cs
@@ -1,26 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using RideX.Services;
 
-namespace RideX.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class UserController : ControllerBase
+namespace RideX.Controllers
 {
-    private readonly IUserService service;
-    public UserController(IUserService service){
-        this.service=service;
-    }
-    [HttpGet]
-    public string name(){
-        return "Sabas";
-    }
-    [HttpGet]
-    public IActionResult GetAll(){
-        var data= this.service.GetUsers();
-        if(data==null){
-            return NotFound();
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService service;
+
+        public UserController(IUserService service)
+        {
+            this.service = service;
         }
-        return Ok(data);
+
+        [HttpGet("name")]
+        public string GetName()
+        {
+            return "Sabas";
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            var data = this.service.GetUsers();
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
     }
 }
